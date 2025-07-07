30bd5ddc25abebf9cd539d45904fbf482ee86825
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-07 18:23:48 +0200

    Integrate `react-router-dom`: set up routing and replace static layout with a dynamic `Dashboard` route. Update dependencies in `package.json` and `package-lock.json`.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,13 @@
-import styled from 'styled-components'
-import GlobalStyles from './styles/GlobalStyles.js'
-import Button from './ui/Button.jsx'
-import Input from './ui/Input.jsx'
-import Heading from './ui/Heading.jsx'
-import Row from './ui/Row.jsx'
-
-const StyledApp = styled.div`
-  padding: 20px;
-`
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard.jsx'
 
 const App = () => {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type={`horizontal`}>
-            <Heading as={`h1`}>The wild oasis!</Heading>
-            <div>
-              <Heading as={`h2`}>check in and out!</Heading>
-              <Button>Check in</Button>
-              <Button variation={`secondary`} size={`small`}>
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row type="vertical">
-            <Heading as={`h3`}>Form</Heading>
-            <form>
-              <Input type="number" placeholder="Enter a number" />
-              <Input type="number" placeholder="Enter a number" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
