38dd830b2ae57bfb3f9f1b92f0eaec7fe9e6c44d
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-08 10:59:38 +0200

    Integrate React Query: add `@tanstack/react-query` and `@tanstack/react-query-devtools` dependencies, configure `QueryClient` in `App.jsx`, and update `package.json` and `package-lock.json`.

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,21 @@ import PageNotFound from './pages/PageNotFound.jsx'
 import Account from './pages/Account.jsx'
 import GlobalStyles from './styles/GlobalStyles.js'
 import AppLayout from './ui/AppLayout.jsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60
+    }
+  }
+})
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -32,7 +43,7 @@ const App = () => {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   )
 }
 
