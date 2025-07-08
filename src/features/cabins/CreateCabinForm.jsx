e18528af7034b5b0d3f1c1635b5a835f9f12ee40
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-08 17:40:36 +0200

    Enhance validation in `CreateCabinForm`: add detailed error messages, validation rules, and field-specific error displays; enable multi-error handling. Add `overflow: auto` to `AppLayout` styling.

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -9,7 +9,6 @@ import { useForm } from 'react-hook-form'
 import { createCabin } from '../../services/apiCabins.js'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import toast from 'react-hot-toast'
-import Spinner from '../../ui/Spinner.jsx'
 
 const FormRow = styled.div`
   display: grid;
@@ -48,7 +47,9 @@ const Error = styled.span`
 `
 
 function CreateCabinForm() {
-  const { register, handleSubmit, reset } = useForm()
+  const { register, handleSubmit, reset, getValues, formState } = useForm()
+
+  const { errors } = formState
 
   const queryClient = useQueryClient()
 
@@ -69,32 +70,72 @@ function CreateCabinForm() {
   const onSubmit = data => {
     mutate(data)
   }
+  const onError = errors => {
+    console.log('Error: ', errors)
+  }
 
   return (
-    <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form onSubmit={handleSubmit(onSubmit, onError)}>
       <FormRow>
         <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" {...register('name', { required: true })} />
+        <Input
+          type="text"
+          id="name"
+          {...register('name', { required: 'This field is required' })}
+        />
+        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" {...register('maxCapacity', { required: true })} />
+        <Input
+          type="number"
+          id="maxCapacity"
+          {...register('maxCapacity', {
+            required: 'This field is required',
+            min: { value: 1, message: 'Capacity must be at least 1 guest' }
+          })}
+        />
+        {errors?.maxCapacity?.message && <Error>{errors.maxCapacity.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" {...register('regularPrice', { required: true })} />
+        <Input
+          type="number"
+          id="regularPrice"
+          {...register('regularPrice', {
+            required: 'This field is required',
+            min: { value: 1, message: 'Minimum capacity is 1 guest' }
+          })}
+        />
+        {errors?.regularPrice?.message && <Error>{errors.regularPrice.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} {...register('discount')} />
+        <Input
+          type="number"
+          id="discount"
+          defaultValue={0}
+          {...register('discount', {
+            required: 'This field is required',
+            validate: value =>
+              value <= getValues().regularPrice || 'Discount should be less than regular price'
+          })}
+        />
+        {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
       </FormRow>
 
       <FormRow>
         <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" {...register('description')} />
+        <Textarea
+          type="number"
+          id="description"
+          defaultValue=""
+          {...register('description', { required: 'This field is required' })}
+        />
+        {errors?.description?.message && <Error>{errors.description.message}</Error>}
       </FormRow>
 
       <FormRow>
