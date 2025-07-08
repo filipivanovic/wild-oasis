10cbe6d2fc6d3c467b156f23806039d455cc33d1
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-08 10:14:02 +0200

    Add placeholder cabin image to `Cabins` page for testing purposes

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -11,6 +11,10 @@ function Cabins() {
   return (
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
+      <img
+        src="https://gxgjesglnrpqkakhmnqp.storage.supabase.co/v1/object/public/cabin-images//cabin-001.jpg"
+        alt=""
+      />
       <p>TEST</p>
     </Row>
   )
