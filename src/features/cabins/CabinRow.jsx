e99dc8df0c58890dc73cdcf1f91e04ee9a098407
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-08 11:55:14 +0200

    Integrate `date-fns`: add dependency to `package.json` and `package-lock.json`, refactor utility functions in `helpers.js` to use `date-fns`, update `CabinRow` to format prices, and enhance `CabinTable` with loading spinner and API data.

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components'
+import { formatCurrency } from '../../utils/helpers.js'
 
 const TableRow = styled.div`
   display: grid;
@@ -10,7 +11,7 @@ const TableRow = styled.div`
   &:not(:last-child) {
     border-bottom: 1px solid var(--color-grey-100);
   }
-`;
+`
 
 const Img = styled.img`
   display: block;
@@ -19,22 +20,40 @@ const Img = styled.img`
   object-fit: cover;
   object-position: center;
   transform: scale(1.5) translateX(-7px);
-`;
+`
 
 const Cabin = styled.div`
   font-size: 1.6rem;
   font-weight: 600;
   color: var(--color-grey-600);
-  font-family: "Sono";
-`;
+  font-family: 'Sono';
+`
 
 const Price = styled.div`
-  font-family: "Sono";
+  font-family: 'Sono';
   font-weight: 600;
-`;
+`
 
 const Discount = styled.div`
-  font-family: "Sono";
+  font-family: 'Sono';
   font-weight: 500;
   color: var(--color-green-700);
-`;
+`
+
+const CabinRow = ({ cabin }) => {
+  const { name, maxCapacity, regularPrice, discount, description, image } = cabin
+
+  return (
+    <TableRow role={`row`}>
+      <img src={image} alt="cabin photo" />
+      <Cabin>{name}</Cabin>
+      <div>Fits up to {maxCapacity} guests</div>
+      <Price>{formatCurrency(regularPrice)}</Price>
+      <Discount>{formatCurrency(discount)}% off</Discount>
+      <div>{description}</div>
+      <button>Delete</button>
+    </TableRow>
+  )
+}
+
+export default CabinRow
