58652a30e5c0bfc26480e29705fa7028df641049
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-08 11:27:00 +0200

    Refactor `Cabins` page: replace placeholder content with `CabinTable` component and remove unused `getCabins` API call.

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,22 +1,18 @@
 import Heading from '../ui/Heading'
 import Row from '../ui/Row'
-import { useEffect } from 'react'
-import { getCabins } from '../services/apiCabins.js'
+import CabinTable from '../features/cabins/CabinTable.jsx'
 
 function Cabins() {
-  useEffect(() => {
-    getCabins().then(res => console.log(res))
-  }, [])
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <img
-        src="https://gxgjesglnrpqkakhmnqp.storage.supabase.co/v1/object/public/cabin-images//cabin-001.jpg"
-        alt=""
-      />
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   )
 }
 
