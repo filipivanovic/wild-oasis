11a35a46780fa2e9a33c8bf16b61251abb02c37b
filipivanovic <filip_ivanovic@hotmail.com>
2025-07-09 13:39:51 +0200

    Refactor cabin data fetching: extract `useCabins` hook and update `CabinTable` to use it.

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,6 +3,7 @@ import { useQuery } from '@tanstack/react-query'
 import { getCabins } from '../../services/apiCabins.js'
 import Spinner from '../../ui/Spinner.jsx'
 import CabinRow from './CabinRow.jsx'
+import { useCabins } from './useCabins.js'
 
 const Table = styled.div`
   border: 1px solid var(--color-grey-200);
@@ -29,14 +30,16 @@ const TableHeader = styled.header`
 `
 
 const CabinTable = () => {
-  const {
-    isLoading,
-    data: cabins,
-    error
-  } = useQuery({
-    queryKey: ['cabins'],
-    queryFn: getCabins
-  })
+  // const {
+  //   isLoading,
+  //   data: cabins,
+  //   error
+  // } = useQuery({
+  //   queryKey: ['cabins'],
+  //   queryFn: getCabins
+  // })
+
+  const { isLoading, cabins } = useCabins()
 
   if (isLoading) return <Spinner />
 
